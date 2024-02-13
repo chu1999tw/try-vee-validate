20e267b444066cbea7d3406d40c36a8e805850da
aaronChang <chuwork.99@gmail.com>
2024-02-13 15:01:10 +0800

    feat: 1. 添加官網範例路由路徑

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,34 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import {createRouter, createWebHashHistory} from 'vue-router'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: () => import("../view/index.vue"),
+      path: '/',
+      name: 'home',
+      component: () => import('../view/index.vue'),
     },
     {
-      path: "/form/create",
-      name: "create",
-      component: () => import("../view/Form/CreateFormView.vue"),
+      path: '/form/create',
+      name: 'create',
+      component: () => import('../view/Form/CreateFormView.vue'),
     },
     {
-      path: "/form/edit",
-      name: "edit",
-      component: () => import("../view/Form/EditFormView.vue"),
+      path: '/form/edit',
+      name: 'edit',
+      component: () => import('../view/Form/EditFormView.vue'),
     },
     {
-      path: "/form/view",
-      name: "view",
-      component: () => import("../view/Form/ViewFormView.vue"),
+      path: '/form/view',
+      name: 'view',
+      component: () => import('../view/Form/ViewFormView.vue'),
+    },
+    {
+      path: '/form/test',
+      name: 'test',
+      component: () => import('../view/Form/TestFormView.vue'),
     },
   ],
-});
+})
 
-export default router;
+export default router
